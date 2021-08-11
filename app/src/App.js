b63e3b209161112ced129e71f452665656be2a6a
amolTelang <telangamol0909@gmail.com>
2021-08-11 20:20:08 +0530

    basic setup

---
+++ app/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+const App=()=>{
+    return(
+        <h1>HEll0 world</h1>
+    )
+}
+export default App;
---
+++ app/src/api/fetchWeatherData.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+
+const URL='some url'
+const fetchWeatherData=async(query)=>{
+    const response =await axios.get(URL)
+}
