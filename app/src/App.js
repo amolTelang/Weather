07273c0128e18c1369554630cd8c1a9a4c947b1d
amolTelang <telangamol0909@gmail.com>
2021-08-12 12:21:12 +0530

    added weather icon' '

--- app/src/App.js
+++ app/src/App.js
@@ -33,6 +33,10 @@ const App=()=>{
                         {Math.round(weather.main.temp)}
                         <sup>&deg;C</sup>
                 </div>
+                <div className="info">
+                        <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+                        <p>{weather.weather[0].description}</p>
+                </div>
             </div>
         )}
         </div>
